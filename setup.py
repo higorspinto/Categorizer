eed9720feb7a0a36b70f90281f3d6a3a37f1fd09
higorspinto <higorspinto@gmail.com>
2019-08-02 15:52:39 -0400

    Including requirements.txt

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="categorizer-higorspinto",
+    name="categorizer",
     version="0.0.1",
     author="Higor dos Santos Pinto",
     author_email="higorspinto@gmail.com",
-    description="A small example package",
+    description="The package reads categories from file and produces a list of frequent categories",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/higorspinto/Categorizer",
